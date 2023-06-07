9c9c6aa0fe1e012a94c4103c532a9aa29754e625
Hemant Sachdeva <hemant.evolver@gmail.com>
2023-06-08 00:35:21 +0530

    Refactor/Style Category list view
    Add sorting by name, pagination, differentiate expense and income by color

--- ExpenseTracker/Models/Category.cs
+++ ExpenseTracker/Models/Category.cs
@@ -17,5 +17,8 @@ namespace ExpenseTracker.Models
 
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
+
+        [NotMapped]
+        public string? TitleWithIcon => Icon + " " + Title;
     }
 }
